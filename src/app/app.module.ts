9dde27afe22221de275c579e5e23e3221976fa69
zeprexar <tawfik.elmsafer@gmail.com>
2020-07-08 12:02:01 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    AngularFireModule.initializeApp({
+  apiKey: "AIzaSyB-GScpLDAeMLL14R5JwpChf8QGvOOYEpk",
+  authDomain: "mayo-223f6.firebaseapp.com",
+  databaseURL: "https://mayo-223f6.firebaseio.com",
+  projectId: "mayo-223f6",
+  storageBucket: "mayo-223f6.appspot.com",
+  messagingSenderId: "1015078063727",
+  appId: "1:1015078063727:web:2800616fc9c19ef3dd479a",
+  measurementId: "G-QJZXR09PK4"
+})],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
